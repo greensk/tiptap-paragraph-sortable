cadb5807358572170e3b2dbe5318d3de2789d255
Grigory Koscheev <grigory.koscheev@gmail.com>
2024-05-31 11:02:31 +0500

    use a custom paragraph node

---
+++ src/ParagraphNode.js
@@ -0,0 +1,27 @@
+import { mergeAttributes } from "@tiptap/core";
+import { Paragraph } from "@tiptap/extension-paragraph";
+
+export const EmrParagraph = Paragraph.extend({
+  addOptions() {
+    return {
+      HTMLAttributes: {}
+    };
+  },
+
+  group: "block",
+  content: "inline*",
+  renderHTML({ node, HTMLAttributes }) {
+    if (HTMLAttributes.id) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      node.attrs.id = HTMLAttributes.id;
+    }
+    return ["p", mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0];
+  },
+
+  addKeyboardShortcuts() {
+    return {
+      "Mod-Alt-0": () => this.editor.commands.setParagraph()
+    };
+  }
+});
--- src/Tiptap.jsx
+++ src/Tiptap.jsx
@@ -1,9 +1,15 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { EmrParagraph } from './ParagraphNode'
 
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit.configure({
+        paragraph: false
+      }),
+      EmrParagraph
+    ],
     content: "<p>Hello World! 🌎️</p>",
   });
 
