533cab6fe427e7213760015972fac50dfe58b870
Grigory Koscheev <grigory@kosheev.com>
2024-06-10 19:39:05 +0700

    improve page sizing

--- src/PageView.jsx
+++ src/PageView.jsx
@@ -1,17 +1,22 @@
 import { NodeViewWrapper, NodeViewContent } from '@tiptap/react'
 import React from 'react'
 
+import formats from './formats'
+
 import './PageView.css'
 
 export default (props) => {
+  const format = formats[props.editor.state.doc.attrs.format]
   return (
     <NodeViewWrapper
       className={`page-view ${props.editor.state.doc.attrs.format}`}
       as="div"
       style={
         {
-          'columnsCount': Number(props.editor.state.doc.attrs.pages),
-          width: String(Number(props.editor.state.doc.attrs.pages) * 300 ) + 'px'
+          'columns-count': Number(props.editor.state.doc.attrs.pages),
+          'column-width': `${format.width}px`,
+          width: String(Number(props.editor.state.doc.attrs.pages) * (format.width + 22) ) + 'px',
+          ...(format.height !== null ? { height: `${format.height}px` } : {})
         }
       }
       data-id={ props.node.attrs.id }
---
+++ src/formats.js
@@ -0,0 +1,14 @@
+export default {
+  'A4': {
+    width: 300,
+    height: 300
+  },
+  'A3': {
+    width: 400,
+    height: 400
+  },
+  'Infinite': {
+    width: 500,
+    height: null
+  }
+}
