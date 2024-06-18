be4827824e7c0aa525cc7ccd3aff42055273aa36
Grigory Koscheev <grigory@kosheev.com>
2024-06-18 16:13:46 +0700

    handle header modification

--- src/HeaderView.jsx
+++ src/HeaderView.jsx
@@ -1,10 +1,30 @@
 import { NodeViewWrapper, NodeViewContent } from '@tiptap/react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './HeaderView.css'
 
 export default (props) => {
   const [editable, setEditable] = useState(false)
+
+  useEffect(() => {
+    const handler = () => {
+      if (!editable) {
+        return
+      }
+      props.editor.state.doc.forEach((pageNode, offset, pageIndex) => {
+        if (offset + 1 === props.getPos()) {
+          console.log(JSON.stringify(props.editor.getJSON()))
+        }
+      })
+    }
+
+    props.editor.on('update', handler)
+
+    return () => {
+      props.editor.off('update', handler)
+    }
+  }, [props.editor, editable])
+
   return (
     <NodeViewWrapper
       className="page-view"
@@ -12,11 +32,12 @@ export default (props) => {
     >
       <div
         onClick={() => {
+          console.log('SET EDITABLE')
           setEditable(true)
         }}
         onBlur={() => {
           console.log('on blur')
-          setEditable(false)
+          // setEditable(false)
         }}
       >
         <NodeViewContent
