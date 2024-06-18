50e4627a94d9435967d2e1ae48fe92a9c4f49b06
Grigory Koscheev <grigory@kosheev.com>
2024-06-18 13:29:28 +0700

    editing header core

--- src/PageView.jsx
+++ src/PageView.jsx
@@ -14,7 +14,7 @@ export default (props) => {
   return (
     <NodeViewWrapper className="page-view" as="div">
       <div>
-        <div>
+        <div contentEditable={ false }>
           <TiptapNested
             content={ content }
           />
