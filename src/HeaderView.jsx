eecdc6fbb015c5fbbec8ea9924f89d443fc8dbf6
Grigory Koscheev <grigory@kosheev.com>
2024-06-18 15:26:09 +0700

    set header editable when clicking

--- src/HeaderView.jsx
+++ src/HeaderView.jsx
@@ -1,13 +1,28 @@
 import { NodeViewWrapper, NodeViewContent } from '@tiptap/react'
-import React from 'react'
+import React, { useState } from 'react'
 
 import './HeaderView.css'
 
 export default (props) => {
+  const [editable, setEditable] = useState(false)
   return (
-    <NodeViewWrapper className="page-view" as="div">
-      <div contentEditable={ false }>
-        <NodeViewContent as="div"></NodeViewContent>
+    <NodeViewWrapper
+      className="page-view"
+      as="div"
+    >
+      <div
+        onClick={() => {
+          setEditable(true)
+        }}
+        onBlur={() => {
+          console.log('on blur')
+          setEditable(false)
+        }}
+      >
+        <NodeViewContent
+          as="div"
+          contentEditable={ editable }
+        ></NodeViewContent>
       </div>
     </NodeViewWrapper>
   )
