0cf452ff17f12eb2729117c2454fd8d0ea6d1904
Grigory Koscheev <grigory@kosheev.com>
2024-06-19 12:21:04 +0700

    disable editable when out of focus

--- src/HeaderView.jsx
+++ src/HeaderView.jsx
@@ -11,43 +11,18 @@ export default (props) => {
       if (!editable) {
         return
       }
-      /*
-      let chain = props.editor.chain()
-      let appendOffset = 0
-      let content
-      let contentLength
-      props.editor.state.doc.forEach((pageNode, pageOffset, pageIndex) => {
-        if (pageOffset + 1 === props.getPos()) {
-          pageNode.forEach((subNode, subNodeOffset) => {
-            if (subNode.type.name === 'header') {
-              content = subNode.toJSON().content
-              contentLength = subNode.nodeSize
-            }
-          })
-        }
-      })
-      props.editor.state.doc.forEach((pageNode, pageOffset, pageIndex) => {
-        if (pageOffset + 1 !== props.getPos()) {
-          pageNode.forEach((subNode, subNodeOffset) => {
-            if (subNode.type.name === 'header') {
-              const from = pageOffset + subNodeOffset + 1 + appendOffset
-              const to = pageOffset + subNodeOffset + subNode.nodeSize - 1 + appendOffset
-              chain = chain.deleteRange({ from, to })
-              chain = chain.insertContentAt(pageOffset + subNodeOffset + 1, content)
-              appendOffset += contentLength - (to - from)
-              console.log({ from, to, content })
-            }
-          })
-        }
-      })
-      chain.run()
-      */
+      const pos = props.getPos()
+      const selection = props.editor.state.selection
+      const focused = selection.$cursor.pos >= pos && selection.$cursor.pos <= pos + props.node.nodeSize
+      if (!focused) {
+        setEditable(false)
+      }
     }
 
-    props.editor.on('update', handler)
+    props.editor.on('selectionUpdate', handler)
 
     return () => {
-      props.editor.off('update', handler)
+      props.editor.off('selectionUpdate', handler)
     }
   }, [props.editor, editable])
 
